53a6c5452f2e4e609088932c0687166a924a1db6
kayvera <mikaylarvr@gmail.com>
2019-04-03 21:41:58 -0700

    second commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,34 +10,35 @@ bottom of the screen, the player loses a ship. If the players loses
 three ships, the game ends.
 """
 
-import sys
-
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
-def run_game(): 
-	# Initialize pygame, settings, and screen object
-	pygame.init()
-	ai_settings = Settings()
-	screen = pygame.display.set_mode(
-		(ai_settings.screen_width, ai_settings.screen_height))
-	pygame.display.set_caption("Alien Invasion")
-	
-	# Set the background color
-	bg_color = (230, 230, 230)
-	
-	# Start the main loop for the game
-	while True:
-		# Watch for keyboard and mouse events
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit() #exits the game
-	
-		# Redraw the screen during each pass through the loop
-		screen.fill(ai_settings.bg_color)
-	
-		# Make the most recently drawn screen visible
-		pygame.display.flip() #makes most recently drawn screen visible
+def run_game():
+    # Initialize pygame, settings, and screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    
+    # Set the background color.
+    bg_color = (230, 230, 230)
+    
+    # Make a ship.
+    ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
+
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
+	
