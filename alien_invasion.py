dc97de19ad2b397cd39277921217b7cf5048f04b
kayvera <mikaylarvr@gmail.com>
2019-04-07 23:16:30 -0700

    4/7 Commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,20 +1,11 @@
-"""
-In Alien Invasion, the player controls a ship that appears at the bottom
-center if the screen. The player can move the shit right and left
-using the the arrows keys and shoot bullets using the spacebar. When the
-game begins, a fleet of aliens fills the sky and moves across and down
-the screen. The player shoots and destroys the aliens. If the player 
-shoots all the aliens, a new fleet appears that moves faster than the 
-previous fleet. If any aliens hits the player's ship or reaches the 
-bottom of the screen, the player loses a ship. If the players loses
-three ships, the game ends.
-"""
-
 import pygame
 from pygame.sprite import Group
 
 from settings import Settings
+from game_stats import GameStats
+from scoreboard import Scoreboard
 from ship import Ship
+from button import Button
 import game_functions as gf
 
 def run_game():
@@ -25,20 +16,37 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     
+    # Make the play button
+    play_button = Button(ai_settings, screen, "Play")
+    
+    # Create an instance to store game statistics and create a scoreboard
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    
     # Set the background color.
     bg_color = (230, 230, 230)
     
-    # Make a ship.
+    # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
+    
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship,
+            aliens, bullets)
+        
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, 
+                bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens,
+            bullets, play_button)
 
 run_game()
-	
+
--- ship.py
+++ ship.py
@@ -23,6 +23,10 @@ class Ship():
         self.moving_right = False
         self.moving_left = False
         
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.center = self.screen_rect.centerx
+        
     def update(self):
         """Update the ship's position, based on movement flags."""
         # Update the ship's center value, not the rect.
